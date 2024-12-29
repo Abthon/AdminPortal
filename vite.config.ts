562291eb3cc38e754bc3b1a6f9cc43e1eb45963e
Abenezer Walelign <walelignabenezer@gmail.com>
2024-12-29 16:50:46 +0300

    Add HTTPS support and update dependencies in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { fileURLToPath, URL } from 'node:url';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import tailwindcss from 'tailwindcss';
+import fs from 'fs';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
@@ -18,5 +20,11 @@ export default defineConfig({
   },
   build: {
     chunkSizeWarningLimit: 3000
+  },
+  server: {
+     https: {
+      key: fs.readFileSync(path.resolve(__dirname, 'localhost.key')),
+      cert: fs.readFileSync(path.resolve(__dirname, 'localhost.crt')),
+    },
   }
 });
