bf375ff66c120384f38ea096efb98756aebd9309
Abenezer Walelign <walelignabenezer@gmail.com>
2025-02-11 10:39:35 +0300

    refactor: changed base url from test before pushing

--- src/auth/_helpers.ts
+++ src/auth/_helpers.ts
@@ -67,7 +67,7 @@ export function setupAxios(axiosInstance: any) {
         if (auth?.refreshToken) {
           try {
             const { data } = await axios.post(
-              "https://static.129.134.201.195.clients.your-server.de/test/api/v1/auth/refresh",
+              "https://static.129.134.201.195.clients.your-server.de/prod/api/v1/auth/refresh",
               {
                 firebaseToken: "1234",
               },
@@ -98,7 +98,7 @@ export function setupAxios(axiosInstance: any) {
 }
 
 const axiosInstance = axios.create({
-  baseURL: "https://static.129.134.201.195.clients.your-server.de/test",
+  baseURL: "https://static.129.134.201.195.clients.your-server.de/prod",
   // baseURL: 'https://static.129.134.201.195.clients.your-server.de/dev'
   // baseURL: 'http://195.201.134.129/prod', // This is the base URL
 });
