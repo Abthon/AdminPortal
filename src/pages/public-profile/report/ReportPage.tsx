d29ec4de7d4006cbfe256560b9cab0df09d51686
Abenezer Walelign <walelignabenezer@gmail.com>
2025-04-01 09:47:27 +0300

    `Added back button to ReportPage`

--- src/pages/public-profile/report/ReportPage.tsx
+++ src/pages/public-profile/report/ReportPage.tsx
@@ -12,12 +12,26 @@ const ReportPage = () => {
   return (
     <Fragment>
       <Container>
-        <h1 className="text-xl font-medium leading-none text-gray-900 mb-6 pl-2">
-          Report
-        </h1>
-        <Navbar>
-          <PageMenu />
-        </Navbar>
+        <button
+          onClick={() => window.history.back()}
+          className="flex text-gray-600 hover:text-gray-900 mr-4 mb-4 pb-4"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="w-6 h-6 mr-2"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M10 19l-7-7m0 0l7-7m-7 7h18"
+            />
+          </svg>
+          {/* Back */}
+        </button>
       </Container>
 
       <Container>
