ea03062cd12564ce44056bed0d3d00e66edda16f
Abenezer Walelign <walelignabenezer@gmail.com>
2025-06-02 14:11:21 +0300

    fix: changed busy marker color to gray

--- src/pages/dashboards/demo1/light-sidebar/blocks/Markers.tsx
+++ src/pages/dashboards/demo1/light-sidebar/blocks/Markers.tsx
@@ -34,7 +34,7 @@ const CustomMarker: React.FC<MarkerProps> = ({
 
   if (driver?.is_online) {
     if (driver?.isBusy) {
-      markerColor = "#0000FF"; // Orange for busy
+      markerColor = "#808080"; // Orange for busy
     } else {
       markerColor = "#008000"; // Green for online regardless of availablity
     }
