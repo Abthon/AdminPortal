8e4d0f02b81322e985d76bfde65fd12a0a869150
Abenezer Walelign <walelignabenezer@gmail.com>
2025-11-02 13:06:37 +0300

    changed environment to test

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -3,5 +3,5 @@ const API_URL = import.meta.env.VITE_APP_API_URL;
 export const getApiUrl = (path: string) => {
   // Remove leading slash if present
   const cleanPath = path.startsWith("/") ? path.slice(1) : path;
-  return `${API_URL}/dev/${cleanPath}`;
+  return `${API_URL}/test/${cleanPath}`;
 };
